275b6d728224aa691cff56a976c3cb2d9233f3a6
KarolPulawski <karol.j.pulawski@gmail.com>
2019-11-12 20:52:23 +0100

    Initial commit

---
+++ src/main/java/com/pulawskk/bettingsite/BettingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.pulawskk.bettingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettingSiteApplication.class, args);
+    }
+
+}
