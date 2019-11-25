44c172a4ccc743aae58c276a949ec196d82bca7b
KarolPulawski <karol.j.pulawski@gmail.com>
2019-11-25 23:15:57 +0100

    Create service layer for bets placing. #44.

---
+++ src/main/java/com/pulawskk/bettingsite/services/BetPlacementService.java
@@ -0,0 +1,7 @@
+package com.pulawskk.bettingsite.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface BetPlacementService {
+}
---
+++ src/main/java/com/pulawskk/bettingsite/services/impl/BetPlacementServiceImpl.java
@@ -0,0 +1,6 @@
+package com.pulawskk.bettingsite.services.impl;
+
+import com.pulawskk.bettingsite.services.BetPlacementService;
+
+public class BetPlacementServiceImpl implements BetPlacementService {
+}
