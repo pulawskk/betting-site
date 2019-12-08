1633b82af8815486d6085669c8580c45078b5a93
KarolPulawski <karol.j.pulawski@gmail.com>
2019-12-08 20:00:41 +0100

    BetSlips are being resulted. Closes #60.

--- src/main/java/com/pulawskk/bettingsite/services/impl/SettlementServiceImpl.java
+++ src/main/java/com/pulawskk/bettingsite/services/impl/SettlementServiceImpl.java
@@ -2,13 +2,12 @@ package com.pulawskk.bettingsite.services.impl;
 
 import com.pulawskk.bettingsite.entities.Bet;
 import com.pulawskk.bettingsite.entities.BetLeg;
+import com.pulawskk.bettingsite.entities.BetSlip;
 import com.pulawskk.bettingsite.entities.Game;
+import com.pulawskk.bettingsite.enums.BetSlipStatus;
 import com.pulawskk.bettingsite.enums.BetStatus;
 import com.pulawskk.bettingsite.enums.ResultType;
-import com.pulawskk.bettingsite.services.BetLegService;
-import com.pulawskk.bettingsite.services.BetService;
-import com.pulawskk.bettingsite.services.GameService;
-import com.pulawskk.bettingsite.services.SettlementService;
+import com.pulawskk.bettingsite.services.*;
 import com.pulawskk.bettingsite.utils.ResultUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -22,11 +21,13 @@ public class SettlementServiceImpl implements SettlementService, ResultUtils {
 
     private final BetService betService;
     private final BetLegService betLegService;
+    private final BetSlipService betSlipService;
     private final GameService gameService;
 
-    public SettlementServiceImpl(BetService betService, BetLegService betLegService, GameService gameService) {
+    public SettlementServiceImpl(BetService betService, BetLegService betLegService, BetSlipService betSlipService, GameService gameService) {
         this.betService = betService;
         this.betLegService = betLegService;
+        this.betSlipService = betSlipService;
         this.gameService = gameService;
     }
 
@@ -57,7 +58,7 @@ public class SettlementServiceImpl implements SettlementService, ResultUtils {
 
     private Boolean isAnyUnresultedBet(List<Bet> bets) {
         return bets.stream()
-                .map(bet -> bet.getResult())
+                .map(Bet::getResult)
                 .anyMatch(Objects::isNull);
     }
 
@@ -71,9 +72,47 @@ public class SettlementServiceImpl implements SettlementService, ResultUtils {
                 .allMatch(b -> ResultType.WIN.equals(b.getResult()));
     }
 
+    @Transactional
     @Override
     public void runBetSlipChecking() {
+        List<BetSlip> betSlips = betSlipService.findAllUnresulted();
+        betSlips.forEach(betSlip -> {
+            List<BetLeg> betLegs = betSlip.getBetLegs();
 
+            if(isAnyLostBetLeg(betLegs)) {
+                betSlip.setResult(ResultType.LOSE);
+                betSlip.setBetSlipStatus(BetSlipStatus.RESULTED);
+                System.out.println("bet slip is lost");
+            }
+
+            if(isAnyUnresultedBetLeg(betLegs)) {
+                System.out.println("bet slip is still unresulted");
+                return;
+            }
+
+            if(isAllBetLegsWin(betLegs)) {
+                betSlip.setResult(ResultType.WIN);
+                betSlip.setBetSlipStatus(BetSlipStatus.RESULTED);
+                System.out.println("bet slip is win");
+            }
+            betSlipService.save(betSlip);
+        });
+    }
+
+    private boolean isAnyUnresultedBetLeg(List<BetLeg> betLegs) {
+        return betLegs.stream()
+                .map(BetLeg::getResult)
+                .anyMatch(Objects::isNull);
+    }
+
+    private boolean isAnyLostBetLeg(List<BetLeg> betLegs) {
+        return betLegs.stream()
+                .anyMatch(bl -> ResultType.LOSE.equals(bl.getResult()));
+    }
+
+    private boolean isAllBetLegsWin(List<BetLeg> betLegs) {
+        return betLegs.stream()
+                .allMatch(bl -> ResultType.WIN.equals(bl.getResult()));
     }
 
     @Override
@@ -93,9 +132,10 @@ public class SettlementServiceImpl implements SettlementService, ResultUtils {
         });
     }
 
-    @Scheduled(cron = "10/20 0/1 * * * ?")
+    @Scheduled(cron = "45 0/1 * * * ?")
     void scheduleRunning() {
         System.out.println("start checking");
         runBetLegChecking();
+        runBetSlipChecking();
     }
 }
