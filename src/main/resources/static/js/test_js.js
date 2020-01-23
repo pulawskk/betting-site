67f27a8123bc638ae77af08c8eea8324b685817f
KarolPulawski <karol.j.pulawski@gmail.com>
2020-01-23 22:41:13 +0100

    Move info about current logged in user to top bar. #78, close #90.

--- src/main/resources/static/js/test_js.js
+++ src/main/resources/static/js/test_js.js
@@ -1,31 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     const button = document.getElementsByClassName("test_button")[0];
-    const text = document.getElementsByName("test_area")[0];
-
-    const button_one = document.getElementsByName("button_add_one")[0];
-    const button_two = document.getElementsByName("button_add_two")[0];
-
     button.addEventListener("click", myFunction);
-    button_one.addEventListener("click", myFunction1);
-    button_two.addEventListener("click", myFunction2);
-
     function myFunction() {
         alert ("Hello World!" + text.innerText);
     }
-    console.dir(text);
 
-    function myFunction1() {
-        button_one.innerText = "clicked";
-        button_two.innerText = "click me";
-        text.innerText = button_one.value;
-    }
-    console.dir(button_one);
+    const text = document.getElementsByName("test_area")[0];
 
-    function myFunction2() {
-        button_two.innerText = "clicked";
-        button_one.innerText = "click me";
-        text.innerText = button_two.value;
+    function addOddToListening() {
+
+        const button_odds = document.getElementsByTagName("button");
+        console.dir(button_odds);
+        for (let i = 0; i < button_odds.length; i++) {
+            button_odds[i].addEventListener("click", function () {
+                alert(button_odds[i].innerText)
+            })
+        }
     }
-    console.dir(button_two);
+
+    window.addEventListener("load", function () {
+        addOddToListening();
+    });
+
 });
 
