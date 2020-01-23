bdba4508955165ae809a5f61d0e7a89257aa2d1a
KarolPulawski <karol.j.pulawski@gmail.com>
2020-01-24 00:22:43 +0100

    Create area for betslips and chats. Code js script to add bet to betslip #78, #89 #91.

--- src/main/resources/static/js/test_js.js
+++ src/main/resources/static/js/test_js.js
@@ -1,26 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    const button = document.getElementsByClassName("test_button")[0];
-    button.addEventListener("click", myFunction);
-    function myFunction() {
-        alert ("Hello World!" + text.innerText);
+
+    const button_odds = document.getElementsByTagName("button");
+    for (let i = 0; i < button_odds.length; i++) {
+        button_odds[i].addEventListener("click", function () {
+            // alert(button_odds[i].innerText)
+
+            const betSlipContent = document.getElementsByClassName("betslip-content")[0];
+            const betDiv = document.createElement("div");
+            betDiv.innerText = "test";
+            betSlipContent.appendChild(betDiv);
+            console.dir(betSlipContent);
+
+
+            const chatContent = document.getElementsByClassName("chat-content");
+        })
     }
 
-    const text = document.getElementsByName("test_area")[0];
-
-    function addOddToListening() {
-
-        const button_odds = document.getElementsByTagName("button");
-        console.dir(button_odds);
-        for (let i = 0; i < button_odds.length; i++) {
-            button_odds[i].addEventListener("click", function () {
-                alert(button_odds[i].innerText)
-            })
-        }
-    }
-
-    window.addEventListener("load", function () {
-        addOddToListening();
-    });
 
 });
 
