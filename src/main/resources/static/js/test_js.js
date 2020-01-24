6c608f27f6e2850cf0e794fb0a41d36ca99d614e
KarolPulawski <karol.j.pulawski@gmail.com>
2020-01-25 00:41:10 +0100

    Refactor right content when new bets appear. #78, #89 close #92.

--- src/main/resources/static/js/test_js.js
+++ src/main/resources/static/js/test_js.js
@@ -3,16 +3,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const button_odds = document.getElementsByTagName("button");
     for (let i = 0; i < button_odds.length; i++) {
         button_odds[i].addEventListener("click", function () {
-            // alert(button_odds[i].innerText)
 
             const betSlipContent = document.getElementsByClassName("betslip-content")[0];
             const betDiv = document.createElement("div");
             betDiv.innerText = "test";
             betSlipContent.appendChild(betDiv);
-            console.dir(betSlipContent);
-
-
-            const chatContent = document.getElementsByClassName("chat-content");
         })
     }
 
