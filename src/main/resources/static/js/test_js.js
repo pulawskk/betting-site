238aec612a64943ad75b204c0f0d7527a02fc1b9
KarolPulawski <karol.j.pulawski@gmail.com>
2020-01-25 21:52:27 +0100

    Pass values regarding selection and market in button with an attributes. #78, #91.

--- src/main/resources/static/js/test_js.js
+++ src/main/resources/static/js/test_js.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         console.dir(but);
         if (but.name.includes("odd_button_", 0)) {
-            button_odds[i].addEventListener("click", function () {
+            but.addEventListener("click", function () {
 
                 const betSlipContent = document.getElementsByClassName("betslip-content")[0];
                 const betDiv = document.createElement("div");
 
-                const betTable = tableBetCreate();
+                const betTable = tableBetCreate(but);
                 betDiv.appendChild(betTable);
                 betSlipContent.appendChild(betDiv);
             });
@@ -20,21 +20,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 });
 
-function tableBetCreate() {
-    var tbl = document.createElement("table");
+function tableBetCreate(oddButton) {
+    const tbl = document.createElement("table");
     tbl.style.width = "90%";
     tbl.setAttribute("border", "1");
 
-    var tblBody = document.createElement("tbody");
-    for (var i = 0; i < 2; i++) {
-        var tr = document.createElement("tr");
-        for (var j = 0; j < 2; j++) {
-            var td = document.createElement("td");
-            td.appendChild(document.createTextNode("test " + i + "-" + j));
-            tr.appendChild(td);
-        }
-        tblBody.appendChild(tr)
-    }
+    const tblBody = document.createElement("tbody");
+
+    const tr0 = document.createElement("tr");
+    const td01 = document.createElement("td");
+    td01.innerText = oddButton.getAttribute("data-el_competition");
+    tr0.appendChild(td01);
+    const td02 = document.createElement("td");
+    td02.innerText = oddButton.getAttribute("data-el_start-date");
+    tr0.appendChild(td02);
+    tblBody.appendChild(tr0);
+
+    const tr1 = document.createElement("tr");
+    const td11 = document.createElement("td");
+    td11.innerText = oddButton.getAttribute("data-el_event-name");
+    tr1.appendChild(td11);
+    const td12 = document.createElement("td");
+    td12.innerText = oddButton.getAttribute("data-el_market-type");
+    tr1.appendChild(td12);
+    tblBody.appendChild(tr1);
+
+    const tr2 = document.createElement("tr");
+    const td21 = document.createElement("td");
+    td21.innerText = oddButton.getAttribute("data-el_selection");
+    tr2.appendChild(td21);
+    const td22 = document.createElement("td");
+    td22.innerText = oddButton.getAttribute("data-el_user-type");
+    tr2.appendChild(td22);
+    tblBody.appendChild(tr2);
+
     tbl.appendChild(tblBody);
     return tbl;
 }
