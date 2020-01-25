4cc30833084e11f4095d497697ac08be76c7b945
KarolPulawski <karol.j.pulawski@gmail.com>
2020-01-25 17:32:40 +0100

    Fix buttons without 'odd_button_' in were being added to betslip. #78, #91.

--- src/main/resources/static/js/test_js.js
+++ src/main/resources/static/js/test_js.js
@@ -2,15 +2,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     const button_odds = document.getElementsByTagName("button");
     for (let i = 0; i < button_odds.length; i++) {
-        button_odds[i].addEventListener("click", function () {
+        const but = button_odds[i];
 
-            const betSlipContent = document.getElementsByClassName("betslip-content")[0];
-            const betDiv = document.createElement("div");
-            betDiv.innerText = "test";
-            betSlipContent.appendChild(betDiv);
-        })
+        console.dir(but);
+        if (but.name.includes("odd_button_", 0)) {
+            button_odds[i].addEventListener("click", function () {
+
+                const betSlipContent = document.getElementsByClassName("betslip-content")[0];
+                const betDiv = document.createElement("div");
+                betDiv.innerText = "test";
+                betSlipContent.appendChild(betDiv);
+            });
+        }
     }
-
 
 });
 
