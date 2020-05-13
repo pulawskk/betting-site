bd5286f613ced08ff25d478e4670d5e11954c4e8
KarolPulawski <karol.j.pulawski@gmail.com>
2020-05-13 18:27:51 +0200

    #109 Cover monthly bonus rules

--- src/main/java/com/pulawskk/bettingsite/WebAppConfig.java
+++ src/main/java/com/pulawskk/bettingsite/WebAppConfig.java
@@ -3,11 +3,19 @@ package com.pulawskk.bettingsite;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/monthlyBonus").setViewName("monthlyBonusDecorated");
     }
 }
